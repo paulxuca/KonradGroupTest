a5316aa9350a15a086ea56e38391eaa6f8e5f85e
paulxuca <paulxuca@gmail.com>
2016-05-13 12:09:29 -0400

    Setting up project

---
+++ server.js
@@ -0,0 +1,28 @@
+var PATH = require('path');
+var express = require('express');
+var app = express();
+var PORT = 8000;
+
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
+var webpack = require('webpack');
+var config = require('./webpack.config');
+var compiler = webpack(config);
+
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+app.use(webpackHotMiddleware(compiler));
+
+
+app.use(express.static(PATH.join(__dirname, 'dist')));
+
+app.get('/', function(request, response) {
+    response.sendFile(__dirname + '/dist/index.html')
+});
+
+app.listen(PORT, function(error) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(`Server running on ${PORT}`);
+    }
+});
