7b18ee1cd63d576831f3589c575ff33d2b5ecb73
paulxuca <paulxuca@gmail.com>
2016-05-14 23:35:41 -0400

    More Refactoring

--- src/components/MainAppContainer.js
+++ src/components/MainAppContainer.js
@@ -4,7 +4,7 @@ import EachGame from './parts/EachGame';
 import DatePicker from './parts/DatePicker';
 
 
-var months = {
+const months = {
     '1': 'Jan',
     '2': 'Feb',
     '3': 'Mar',
@@ -19,47 +19,49 @@ var months = {
     '12': 'Dec',
 }
 
-const MainAppContainer = React.createClass({
-	
-		parseQueryOptions(){
-			if(this.props.query){
-			if(this.props.query.toUpperCase().indexOf('TEAM') != -1 || this.props.query.toUpperCase().indexOf('STADIUM') != -1 ){
-				if(this.props.query.trim().split(':')[1]!= ''){
-					if(this.props.query.trim().split(':')[0].toUpperCase() == 'TEAM'){
-						return {
-						q:{
-							'away':'away_team_name',
-							'home':'home_team_name'
-						},
-						res: this.props.query.split(':')[1].toUpperCase()
-						}
-					}
-					else if(this.props.query.trim().split(':')[0].toUpperCase() == 'STADIUM'){
-						return {
-						q:{
-							'away':'venue',
-							'home':'venue'
-						},
-						res: this.props.query.split(':')[1].toUpperCase()
-						}
-					}
-				}
-			}
-	}
-	return{
-				q: {
-					'away':'away_team_name',
-					'home':'home_team_name',
-				},
-				res: 'BLUE JAYS'
-			}
-	},
-	
+const dateStyle = {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginBottom: '0.5rem'
+}
 
+const MainAppContainer = React.createClass({
+		parseQueryOptions() {
+		    if (this.props.query) {
+		        if (this.props.query.toUpperCase().indexOf('TEAM') != -1 || this.props.query.toUpperCase().indexOf('STADIUM') != -1) {
+		            if (this.props.query.trim().split(':')[1] != '') {
+		                if (this.props.query.trim().split(':')[0].toUpperCase() == 'TEAM') {
+		                    return {
+		                        q: {
+		                            'away': 'away_team_name',
+		                            'home': 'home_team_name'
+		                        },
+		                        res: this.props.query.split(':')[1].toUpperCase()
+		                    }
+		                } else if (this.props.query.trim().split(':')[0].toUpperCase() == 'STADIUM') {
+		                    return {
+		                        q: {
+		                            'away': 'venue',
+		                            'home': 'venue'
+		                        },
+		                        res: this.props.query.split(':')[1].toUpperCase()
+		                    }
+		                }
+		            }
+		        }
+		    }
+		    return {
+		        q: {
+		            'away': 'away_team_name',
+		            'home': 'home_team_name',
+		        },
+		        res: 'BLUE JAYS'
+		    }
+		},
 		render(){
 		var games = this.props.data;
 		var current = moment([Number(this.props.year), Number(this.props.month)-1, Number(this.props.date)]);
-		var date = (this.props.date && this.props.month)?`${months[this.props.month]} ${this.props.date} ${this.props.year}`: 'Pick a Date!'; 
+		var date = (this.props.date && this.props.month)?`${months[this.props.month]} ${this.props.date} ${this.props.year}`: ''; 
 		if(this.props.year == '' && this.props.month == '' && this.props.date == ''){
 			date=("Please Enter a date!")
 		}
@@ -67,11 +69,13 @@ const MainAppContainer = React.createClass({
 				date = 'Invalid Date!';
 		}
 		var query = this.parseQueryOptions();
+
+		var dateHeader = (<h1 style={dateStyle}>{date}</h1>);
 		if(games && games.length){
 			return(
 			<div className="container" style={{textAlign:'center'}}>
 				<div className="row">
-				<h1 style={{marginLeft:'auto', marginRight:'auto', marginBottom:'0.5rem'}}>{date}</h1>
+				{dateHeader}
 				</div>
 				<DatePicker onClick={this.props.onDateSwitch} date={this.props.date}/>
 					{games.map(function(result){
@@ -98,17 +102,23 @@ const MainAppContainer = React.createClass({
 			return(
 				<div className="container" style={{textAlign:'center'}}>
 				<div className="row">
-				<h1 style={{marginLeft:'auto', marginRight:'auto', marginBottom:'0.5rem'}}>{date}</h1>
+				{dateHeader}
 				</div>
 				<DatePicker onClick={this.props.onDateSwitch} date={this.props.date}/>
 				<EachGame onClick={this.props.onClick} result={this.props.data} score_home={this.props.data.linescore.r.home} score_away={this.props.data.linescore.r.away} key={this.props.data.id}/>
 				</div>);
 		}else{
 			var gamesMessage = (this.props.date == "")?'':"No Games Today!";
+			var introduction = (this.props.date =="")?(
+				<div className="row">
+				<p style={{marginLeft:'auto', marginRight:'auto'}}>To find MLB games and more information.</p>
+				</div>
+				):''
 			return(<div className="container" style={{textAlign:'center'}}>
 				<div className="row">
-				<h1 style={{marginLeft:'auto', marginRight:'auto', marginBottom:'0.5rem'}}>{date}</h1>
+				{dateHeader}
 				</div>
+				{introduction}
 				<DatePicker onClick={this.props.onDateSwitch} date={this.props.date}/>
 				<h4>{gamesMessage}</h4>
 				</div>);
